99c0ed71a26b249f6bf2b642f804ba2c7345ca4a
rdx-1766 <pavankumarmattad@gmail.com>
2023-06-14 12:46:48 +0530

    Splash screen done

---
+++ app/src/main/java/com/example/rnsitcollegeapp/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.rnsitcollegeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(SplashScreen.this,R.color.orange));
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        ImageView backgroundImage = findViewById(R.id.SplashScreenImage);
+        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide);
+        backgroundImage.startAnimation(slideAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
